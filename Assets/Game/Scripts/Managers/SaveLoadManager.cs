1f739e6fb5c2450b995fd04c010cc000768f8dae
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2023-06-13 18:01:21 -0300

    criado script para controle de progresso do jogador

---
+++ Assets/Game/Scripts/Managers/SaveLoadManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace StalinKilledMelons.Managers
+{
+    /// <summary>
+    /// Gerencia o salvamento e carregamento do progresso do jogador.
+    /// </summary>
+    public static class SaveLoadManager
+    {
+        /// <summary>
+        /// Salva o progresso do jogador.
+        /// </summary>
+        public static void SavePlayerProgress(PlayerProgressData playerProgressData)
+        {
+            string json = JsonUtility.ToJson(playerProgressData);
+            PlayerPrefs.SetString("PlayerProgress", json);
+        }
+
+        /// <summary>
+        /// Carrega o progresso do jogador.
+        /// </summary>
+        /// <returns>O progresso do jogador.</returns>
+        public static PlayerProgressData LoadPlayerProgress()
+        {
+            string json = PlayerPrefs.GetString("PlayerProgress");
+            if (!string.IsNullOrEmpty(json))
+            {
+                return JsonUtility.FromJson<PlayerProgressData>(json);
+            }
+            else
+            {
+                return new PlayerProgressData();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Dados do progresso do jogador.
+    /// </summary>
+    [System.Serializable]
+    public class PlayerProgressData
+    {
+        public int score; // Pontuação do jogador.
+        public int unlockedLevels; // Níveis desbloqueados.
+
+        // Outras preferências do jogador podem ser adicionadas aqui.
+
+        public PlayerProgressData()
+        {
+            score = 0;
+            unlockedLevels = 1;
+        }
+    }
+}
