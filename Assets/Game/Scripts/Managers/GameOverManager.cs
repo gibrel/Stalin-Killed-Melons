461d0c9009f4e226098ff0c5de4eed7034a92cc4
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2023-06-14 11:18:28 -0300

    criado script para gerenciar o gameover

---
+++ Assets/Game/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace StalinKilledMelons.Managers
+{
+    /// <summary>
+    /// Gerencia o estado de jogo quando o jogador perde.
+    /// </summary>
+    public class GameOverManager : MonoBehaviour
+    {
+        private LevelManager levelManager; // Referência para o LevelManager, responsável por controlar os níveis do jogo.
+
+        private void Awake()
+        {
+            levelManager = FindObjectOfType<LevelManager>(); // Encontra o LevelManager na cena e atribui à variável levelManager.
+        }
+
+        /// <summary>
+        /// Exibe a tela de game over.
+        /// </summary>
+        public void ShowGameOverScreen()
+        {
+            // Implemente aqui a lógica para exibir a tela de game over
+        }
+
+        /// <summary>
+        /// Reinicia o jogo.
+        /// </summary>
+        public void RestartGame()
+        {
+            levelManager.ReloadLevel();
+        }
+
+        /// <summary>
+        /// Retorna ao menu principal.
+        /// </summary>
+        public void ReturnToMainMenu()
+        {
+            levelManager.LoadLevel("MainMenu");
+        }
+    }
+}
